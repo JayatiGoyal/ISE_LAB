50e39de8e8a0cf4b294283904c889990a61483fe
JayatiGoyal <44127709+JayatiGoyal@users.noreply.github.com>
2020-04-16 15:42:07 +0530

    Add files via upload

---
+++ like_button1.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+    window.location.href="http://localhost/lab/studentlogin.html";
+      //return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'LOGOUT'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
